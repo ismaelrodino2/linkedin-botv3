cbe26d4a25311ccd38b408b9e5ca9b2abf11cb2c
ismael <ismael.rodino@hotmail.com>
2024-10-29 21:42:17 -0300

    fix router dom

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 import App from "./App.tsx";
 import "./index.css";
 import { NavBar } from "./components/navbar.tsx";
@@ -9,11 +9,11 @@ import 'react-toastify/dist/ReactToastify.css';
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <HashRouter>
       <NavBar>
         <App />
         <ToastContainer />
       </NavBar>
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>
 );
